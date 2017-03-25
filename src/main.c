070cd0959f29ac2f438cd61b50d2d5390e53f82b
romash1408 <romash1408@yandex.ru>
2017-03-25 08:07:51 +0700

    Test bstree_add() in main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include "bstree.h"
 
-const int N = 2;
+const int N = 32;
 
 int main()
 {
-	struct BSTree *tmp = bstree_create("0", 0);
-	for(int i = 0; i < N; ++i)
+	/*struct BSTree *tmp = bstree_create("а", 0);
+	bstree_print(tmp);
+	
+	for(int i = 1; i <= N; ++i)
 	{
 		char key[3];
-		sprintf(key, "%d", i);
+		key[0] = "а"[0] + i / 16;
+		key[1] = "а"[1] + i % 16;
 		tmp = bstree_add(tmp, key, i);
+		printf("Added %s (%d)!\n", key, i);
 	}
 	if(!tmp)
 	{
@@ -18,6 +22,34 @@ int main()
 		return 1;
 	}
 	bstree_print(tmp);
-	bstree_free(tmp);
+	bstree_free(tmp);*/
+	
+	char *s = "а";
+	for(int i = 0; i < sizeof(s); ++i)
+	{
+		printf("%d ", s[i]);
+	}
+	printf("\n");
+	
+	s = "п";
+	for(int i = 0; i < sizeof(s); ++i)
+	{
+		printf("%d ", s[i]);
+	}
+	printf("\n");
+	
+	s = "р";
+	for(int i = 0; i < sizeof(s); ++i)
+	{
+		printf("%d ", s[i]);
+	}
+	printf("\n");
+	
+	s = "я";
+	for(int i = 0; i < sizeof(s); ++i)
+	{
+		printf("%d ", s[i]);
+	}
+	printf("\n");
 	return 0;
 }
