a759d36ecef64bcf4feac2672d8999bed98eabfa
romash1408 <romash1408@yandex.ru>
2017-04-05 15:28:57 +0700

    Write hashtab_lookup() and hashtab_delete(). Also fix problem with memory free in hashtab_free()

--- src/hashtab.c
+++ src/hashtab.c
@@ -1,6 +1,6 @@
 #include "hashtab.h"
 
-#define GET_HF(_ht) *((HashFunc **)((char *)_ht - sizeof(HashFunc *)))
+#define GET_HF(_ht) ((HashFunc **)((char *)_ht - sizeof(HashFunc *)))
 
 static const unsigned int HASH_SIMPLE_M = 10;
 static unsigned int HASH_SIMPLE_F(const char* key)
@@ -26,7 +26,7 @@ ListNode** hashtab_init(HashFunc *_hf)
 
 void hashtab_add(ListNode **_ht, char *_key, int _val)
 {
-	HashFunc *hf = GET_HF(_ht);
+	HashFunc *hf = *GET_HF(_ht);
 	ListNode **to = _ht + hf->func(_key);
 	while(*to) to = &((*to)->next);
 	*to = malloc(sizeof(ListNode));
@@ -48,18 +48,38 @@ void hashtab_add(ListNode **_ht, char *_key, int _val)
 
 ListNode* hashtab_lookup(ListNode **_ht, char *_key)
 {
-	return *_ht;
+	HashFunc *hf = *GET_HF(_ht);
+	ListNode* ret = _ht[hf->func(_key)];
+	while(ret && strcmp(ret->key, _key) != 0)
+	{
+		ret = ret->next;
+	}
+	return ret;
 }
 
 void hashtab_delete(ListNode **_ht, char *_key)
 {
-	
+	HashFunc *hf = *GET_HF(_ht);
+	ListNode **place = _ht + hf->func(_key), *del;
+	while((del = *place))
+	{
+		if(strcmp(del->key, _key) == 0)
+		{
+			*place = del->next;
+			free(del->key);
+			free(del);
+		}
+		else
+		{
+			place = &del->next;
+		}
+	}
 }
 
 
 void hashtab_print(ListNode **_ht)
 {
-	HashFunc *hf = GET_HF(_ht);
+	HashFunc *hf = *GET_HF(_ht);
 	for(unsigned int i = 0; i < hf->maxVal; ++i)
 	{
 		ListNode *next = _ht[i];
@@ -73,20 +93,20 @@ void hashtab_print(ListNode **_ht)
 	}
 }
 
-static void hashtab_free_node(ListNode *_node)
+static void hashtab_free_nodes(ListNode *_node)
 {
 	if(!_node) return;
-	hashtab_free_node(_node->next);
+	hashtab_free_nodes(_node->next);
 	free(_node->key);
 	free(_node);
 }
 
 void hashtab_free(ListNode **_ht)
 {
-	HashFunc *hf = GET_HF(_ht);
-	for(unsigned int i = 0; i < hf->maxVal; ++i)
+	HashFunc **hf = GET_HF(_ht);
+	for(unsigned int i = 0; i < (*hf)->maxVal; ++i)
 	{
-		hashtab_free_node(_ht[i]);
+		hashtab_free_nodes(_ht[i]);
 	}
 	free(hf);
 }
